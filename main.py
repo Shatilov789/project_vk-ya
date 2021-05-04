b71fd0671b806ee439ea3795cf40f1ac744d6ef7
Pavel <realsteal1223@gmail.com>
2021-05-04 23:08:38 +0300

    Uodate codec

--- main.py
+++ main.py
@@ -1,73 +1,93 @@
 import requests
 import json
 
-URL = 'https://api.vk.com/method/photos.get'
-params = {
-    'owner_id': '552934290',
-    'album_id': 'profile',
-    'extended': '1',
-    'access_token': '',
-    'v':'5.130',
-}
+def get_dict_vk(id_user_vk, token_vk):
+    URL = 'https://api.vk.com/method/photos.get'
+    params = {
+        'owner_id': f'{id_user_vk}',
+        'album_id': 'profile',
+        'extended': '1',
+        'access_token': f'{token_vk}',
+        'v':'5.130',
+    }
+    res = requests.get(URL, params=params)
+    return  res.json()
 
-folder_name = params["owner_id"]
-res = requests.get(URL, params=params)
-dict_vk = res.json()
+def creat_folder(id_user_vk, def_token_ya):
 
-if 'error' in str(dict_vk):
-    print()
-    print(dict_vk['error']['error_msg'])
+        url_yandex_disk_creat_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
+        headers = {'Connect-type': 'application/json', 'Authorization': f'{def_token_ya}'}
+        params = {"path": f'{id_user_vk}'}
+        res_ya = requests.put(url_yandex_disk_creat_folder, headers=headers, params=params)
+        dict_ya_message = res_ya.json()
 
-if 'response' in str(dict_vk):
-    upload_url = 'https://cloud-api.yandex.net/v1/disk/resources'
-    headers = {'Connect-type': 'application/json', 'Authorization': ''}
-    params = {"path": folder_name}
-    response = requests.put(upload_url, headers=headers, params=params)
+        if 'DiskPathPointsToExistentDirectoryError' in dict_ya_message['error']:
+            return f'Создана папка с именем на YandexDisk:{id_user_vk}'
+        else:
+            return f'Ошибка: {dict_ya_message["message"]} Проверьте корректность данных!'
 
-    len_foto = 0
-    for key in dict_vk['response']['items']:
-        dict_vk['response']['items'][len_foto]['likes']
-        len_foto += 1
-
+def upload_foto(def_dict_vk, def_id_user, def_token_ya):
     counter = 0
     max_foto = 0
     best_url: str
     size_url = []
-    cv2 = []
-    list_likes =[]
+    list_likes = []
     size_foto: str
-
     list_foto = []
     dict_foto = {}
-    upload_url1 = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-    print()
-    print(f'Скачиваем фото с профиля VK: {folder_name}')
-    print("-------------------------------------")
-    print(f'На загрузку получили {len_foto} фото:')
-    for key in dict_vk['response']['items']:
-        xp = dict_vk['response']['items'][counter]['sizes']
-        size_url.append(xp)
-        for val in size_url:
-            for key in val:
-                if int(key['height']) > int(max_foto):
-                    max_foto = key['height']
-                best_url = key['url']
-                size_foto  = key['type']
-        cv2.append(best_url)
-        url = best_url
+    len_foto = 0
 
-        list_likes.append(dict_vk['response']['items'][counter]['likes']['count'])
-        params = {"path": f'{folder_name}/{list_likes[counter]}', "url": url, "overwrite": "true"}
-        response = requests.post(upload_url1, headers=headers, params=params)
-        dict_foto["file_name"] = f'{list_likes[counter]}.jpg'
-        dict_foto["size"] = size_foto
-        list_foto.append(dict_foto)
-        print(f'Фото "{list_likes[counter]}.jpg" отпралено на YandexDisk.\n'
-              f'Фото {counter+1} из {len_foto} ')
+    if 'error' in str(def_dict_vk):
+        print()
+        print(def_dict_vk['error']['error_msg'])
 
-        counter += 1
-    print()
-    print(f'Успешная загрузка, все фото на YandexDisk!')
+    if 'response' in str(def_dict_vk):
 
-    with open('log.txt', "a") as file:
-        file.write(json.dumps(list_foto, sort_keys = True, indent = 4))
+        for key in def_dict_vk['response']['items']:
+            def_dict_vk['response']['items'][len_foto]['likes']
+            len_foto += 1
+
+        url_yandexdisk_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = {'Connect-type': 'application/json', 'Authorization': f'{def_token_ya}'}
+        print()
+        print(f'Скачиваем фото с профиля VK: {def_id_user}')
+        print("-------------------------------------")
+        print(f'На загрузку получили {len_foto} фото:')
+
+        for keys in def_dict_vk['response']['items']:
+            xp = def_dict_vk['response']['items'][counter]['sizes']
+            size_url.append(xp)
+            for val in size_url:
+                for key in val:
+                    if int(key['height']) > int(max_foto):
+                        max_foto = key['height']
+                    best_url = key['url']
+                    size_foto = key['type']
+            url = best_url
+
+            list_likes.append(def_dict_vk['response']['items'][counter]['likes']['count'])
+            params = {"path": f'{def_id_user}/{list_likes[counter]}', "url": url, "overwrite": "true"}
+            requests.post(url_yandexdisk_upload, headers=headers, params=params)
+
+            dict_foto["file_name"] = f'{list_likes[counter]}.jpg'
+            dict_foto["size"] = size_foto
+            list_foto.append(dict_foto)
+            print(f'Фото "{list_likes[counter]}.jpg" отпралено на YandexDisk.\n'
+                  f'Фото {counter + 1} из {len_foto} ')
+
+            counter += 1
+        print()
+        print(f'==>> Успешная загрузка, все фото на YandexDisk!')
+
+        with open('log.txt', "a") as file:
+            file.write(json.dumps(list_foto, sort_keys=True, indent=4))
+
+print("****************************************************")
+id_user = str(input("Введите id пользователя VK: "))
+token_vk = str(input("Введите токен API из VK: "))
+token_ya = str(input("Введите токен API из YandexDisk: "))
+print("****************************************************")
+
+dict_vk = get_dict_vk(id_user, token_vk)
+print(creat_folder(id_user, token_ya))
+upload_foto(dict_vk, id_user, token_ya)
